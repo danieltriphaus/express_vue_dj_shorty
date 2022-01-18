5e34de54ce7d031f89478f1527c6803928c44dc3
danieltriphaus <d.triphaus@outlook.de>
2022-01-18 20:16:15 +0100

    add basic design to landing page

--- src/frontend/views/Host.vue
+++ src/frontend/views/Host.vue
@@ -1,29 +1,28 @@
 <template>
-  <div 
-    :class="this.$route.name"
-    class="container"
-  >
-    <div class="spotify_user_info row">
-      <h3>
-        Eingeloggt als
-        {{ spotifyUser.display_name }}
-      </h3>
+    <div
+        :class="this.$route.name"
+        class="container"
+    >
+        <div class="spotify_user_info row">
+            <h3>
+                Eingeloggt als
+                {{ spotifyUser.display_name }}
+            </h3>
+        </div>
+        <div class="row">
+            <router-link
+                to="/new_music_session"
+                type="button"
+                class="btn btn-outline-primary"
+            >
+                Neue Session erstellen
+            </router-link>
+        </div>
+        <MusicSessionList
+            :music-sessions="musicSessions"
+            :spotify-user-id="spotifyUser.id"
+        />
     </div>
-    <div class="row">
-      <router-link
-        to="/new_music_session"
-        type="button"
-        class="btn btn-outline-primary"
-      >
-        Neue Session erstellen
-      </router-link>
-    </div>
-
-    <MusicSessionList 
-      :music-sessions="musicSessions" 
-      :spotify-user-id="spotifyUser.id" 
-    />
-  </div>
 </template>
 
 <script>
@@ -32,19 +31,19 @@ import MusicSessionList from "../components/MusicSessionList";
 import { getMusicSessions } from "../features/MusicSessions/getMusicSessions";
 
 export default {
-  components: {
-    MusicSessionList,
-  },
-  data() {
-    return {
-      spotifyUser: { display_name: "" },
-      musicSessions: [],
-    };
-  },
-  async created() {
-    this.accessToken = await this.$getAccessToken();
-    this.spotifyUser = await getCurrentSpotifyUser(this.accessToken);
-    this.musicSessions = await getMusicSessions(this.spotifyUser.id);
-  },
+    components: {
+        MusicSessionList,
+    },
+    data() {
+        return {
+            spotifyUser: { display_name: "" },
+            musicSessions: [],
+        };
+    },
+    async created() {
+        this.accessToken = await this.$getAccessToken();
+        this.spotifyUser = await getCurrentSpotifyUser(this.accessToken);
+        this.musicSessions = await getMusicSessions(this.spotifyUser.id);
+    },
 };
 </script>
--- src/frontend/views/Index.vue
+++ src/frontend/views/Index.vue
@@ -1,35 +1,41 @@
 <template>
-  <div class="index">
-    <div v-if="!accessToken">
-      <router-link to="/authorize">
-        Einloggen bei Spotify 
-      </router-link>
+    <div class="index container">
+        <div
+            v-if="!accessToken"
+            class="row justify-content-center mt-4"
+        >
+            <router-link
+                to="/authorize"
+                class="btn btn-outline-primary"
+            >
+                Einloggen bei Spotify
+            </router-link>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "Index",
-  data() {
-    return {
-      spotifyUser: {},
-      accessToken: false,
-    };
-  },
-  async created() {
-    if (this.$route.query.code) {
-      this.$router.push({
-        path: "/authorize",
-        query: this.$route.query
-      });
-    } else {
-      this.accessToken = await this.$getAccessToken();
+    name: "Index",
+    data() {
+        return {
+            spotifyUser: {},
+            accessToken: false,
+        };
+    },
+    async created() {
+        if (this.$route.query.code) {
+            this.$router.push({
+                path: "/authorize",
+                query: this.$route.query,
+            });
+        } else {
+            this.accessToken = await this.$getAccessToken();
 
-      if (this.accessToken) {
-        this.$router.push("/host");
-      }
-    }
-  },
+            if (this.accessToken) {
+                this.$router.push("/host");
+            }
+        }
+    },
 };
 </script>
